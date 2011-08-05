331d3a01dbd4f7ce6bcca7b8ed7da2b20d23c84b
Seth Difley <difley@gmail.com>
2011-08-05 16:36:37 -0500

    xmpp-admin.pl added

---
+++ xmpp-admin.pl
@@ -0,0 +1,50 @@
+# xmpp-admin.pl
+#copy into .irssi/scripts
+#load in irssi with: /script load xmpp-admin
+use Irssi;
+
+$::VERSION='0.0.1';
+%::IRSSI = (
+    authors => 'Seth Difley',
+    contact => '',
+    name => 'xmpp-admin',
+    description => 'Adds admin commands to irssi-xmpp',
+    url => '',
+    license => 'GNU General Public License',
+    changed => '$Date$'
+    );
+
+#/affiliate affiliation_level [jid]
+#If jid is absent, the affiliation list is returned (Raw xml messages must be active to see the list.)
+sub cmd_affiliate {
+    my ($data,$server,$wid) = @_;
+    @items = split(" ", $data);
+    if ($items[0]) {
+        if ($items[1]) {
+            $affil = "QUOTE <iq type=\'set\' id=\'affiliate_set\' to=\'$wid->{name}\'> <query xmlns=\'http://jabber.org/protocol/muc#admin\'> <item jid=\'$items[1]\' affiliation=\'$items[0]\'/> </query> </iq>";
+        }
+        else {
+            $affil = "QUOTE <iq type=\'get\' id=\'affiliate_get\' to=\'$wid->{name}\'> <query xmlns=\'http://jabber.org/protocol/muc#admin\'> <item affiliation=\'$items[0]\'/> </query> </iq>";
+        }
+        $server->command("$affil");
+    }
+    else {
+        Irssi::print("/affiliate none|owner|admin|member|outcast [jid]");
+    }
+}
+
+#/role role_level nickname
+sub cmd_role {
+    my ($data,$server,$wid) = @_;
+    @items = split(" ", $data);
+    if ($items[1]) {
+        $role = "QUOTE <iq type=\'set\' id=\'role_set\' to=\'$wid->{name}\'> <query xmlns=\'http://jabber.org/protocol/muc#admin\'> <item nick=\'$items[1]\' role=\'$items[0]\'/> </query> </iq>";
+        $server->command("$role");
+    }
+    else {
+        Irssi::print("/role none|moderator|participant|visitor nickname");
+    }
+}
+
+Irssi::command_bind('affiliate', \&cmd_affiliate);
+Irssi::command_bind('role', \&cmd_role);
